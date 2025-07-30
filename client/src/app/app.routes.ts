65fd629698e722acbb718b6f88cfad6273ccac5f
Viktor <viktorsarafov1@abv.bg>
2025-07-30 05:18:26 +0300

    Generate About Component and update routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { About } from './features/about/about';
 
 export const routes: Routes = [
     {
@@ -9,5 +10,9 @@ export const routes: Routes = [
     {
         path: 'home',
         loadComponent: () => import('./features/home/home').then(component => component.Home)
+    },
+    {
+        path: 'about',
+        component: About
     }
 ];
