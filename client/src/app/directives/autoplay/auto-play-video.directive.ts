e4eccad446ec6b04a16759c452a6ad4a77543ccd
Viktor <viktorsarafov1@abv.bg>
2025-07-30 03:04:04 +0300

    Implement AutoPlayVideo to enable automatic video playback

---
+++ client/src/app/directives/autoplay/auto-play-video.directive.ts
@@ -0,0 +1,26 @@
+import { Directive, ElementRef, AfterViewInit } from '@angular/core';
+
+@Directive({
+  selector: '[appAutoPlayVideo]',
+})
+export class AutoPlayVideo implements AfterViewInit {
+  private video!: HTMLVideoElement;
+
+  constructor(private el: ElementRef<HTMLVideoElement>) {
+  }
+
+  ngAfterViewInit(): void {
+    this.enableAutoplay();
+  }
+
+  private enableAutoplay(): void {
+    this.video = this.el.nativeElement;
+    console.log(this.video);
+
+    this.video.muted = true;
+
+    this.video.play().catch(err => {
+      alert('Unable to load background video.');
+    });
+  }
+}
---
+++ client/src/app/directives/autoplay/auto-play-video.spec.ts
@@ -0,0 +1,8 @@
+import { AutoPlayVideo } from './auto-play-video';
+
+describe('AutoPlayVideo', () => {
+  it('should create an instance', () => {
+    const directive = new AutoPlayVideo();
+    expect(directive).toBeTruthy();
+  });
+});
