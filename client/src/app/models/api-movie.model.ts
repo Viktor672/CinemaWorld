c93ac07d4afae24fb917f023cf4cddfec735a5bd
Viktor <viktorsarafov1@abv.bg>
2025-08-08 03:55:10 +0300

    Add ApiMovie Interface to handle server data

---
+++ client/src/app/models/api-movie.model.ts
@@ -0,0 +1,10 @@
+export interface ApiMovie {
+    _ownerId: string,
+    title: string,
+    description: string,
+    img: string,
+    genre: string,
+    _createdOn: number,
+    releaseDate: Date
+    _id: string
+}
