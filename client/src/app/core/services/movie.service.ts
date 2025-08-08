7770cfba2fde21ccf855e6db83b9311692b695e2
Viktor <viktorsarafov1@abv.bg>
2025-08-08 04:47:10 +0300

    Upgrade functionality and implement new logic in movie.service.ts

--- client/src/app/core/services/movie.service.ts
+++ client/src/app/core/services/movie.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { map, Observable } from "rxjs";
+import { ApiMovie } from "../../models/api-movie.model";
 import { Movie } from "../../models";
 
 @Injectable({
@@ -12,6 +13,33 @@ export class MovieService {
     constructor(private httpClient: HttpClient) { }
 
     getMovies(): Observable<Movie[]> {
-        return this.httpClient.get<Movie[]>(`${this.baseUrl}/movies`);
+        return this.httpClient.get<ApiMovie[]>(`${this.baseUrl}/movies`).pipe(map((apiMovieArr: ApiMovie[]) =>
+            apiMovieArr.map((apiMovie: ApiMovie) =>
+                this.mapApiMovieToMovie(apiMovie)
+            )));
+    }
+
+    addMovie(title: string, genre: string, description: string, imageUrl: string, releaseDate: Date): Observable<Movie> {
+        let movie = {
+            title,
+            genre,
+            description,
+            imageUrl,
+            releaseDate
+        }
+let accessToken = localStorage.getItem('currentUser');
+        return this.httpClient.post<ApiMovie>(`${this.baseUrl}/movies`, movie).pipe(map((apiMovie: ApiMovie) => this.mapApiMovieToMovie(apiMovie)));
+    }
+
+    private mapApiMovieToMovie(apiMovie: ApiMovie): Movie {
+        return {
+            _ownerId: apiMovie._ownerId,
+            title: apiMovie.title,
+            description: apiMovie.description,
+            imageUrl: apiMovie.img,
+            genre: apiMovie.genre,
+            releaseDate: new Date(apiMovie.releaseDate),
+            _id: apiMovie._id
+        }
     }
 }
