6a30332264e34260bd5366382d576cb60bdf1744
Viktor <viktorsarafov1@abv.bg>
2025-08-09 10:11:37 +0300

    Generate EditMovie Component

---
+++ client/src/app/features/edit-movie/edit-movie.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-movie',
+  imports: [],
+  templateUrl: './edit-movie.html',
+  styleUrl: './edit-movie.css'
+})
+export class EditMovie {
+
+}
