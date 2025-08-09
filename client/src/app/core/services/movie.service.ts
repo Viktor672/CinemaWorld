6ecd074c717fa30815c204b9d65eb093bbb1c853
Viktor <viktorsarafov1@abv.bg>
2025-08-09 12:12:12 +0300

    Add editMovie method in movie.service.ts

--- client/src/app/core/services/movie.service.ts
+++ client/src/app/core/services/movie.service.ts
@@ -38,11 +38,22 @@ export class MovieService {
         return this.httpClient.post<ApiMovie>(`${this.baseUrl}/movies`, movie).pipe(map((apiMovie: ApiMovie) => this.mapApiMovieToMovie(apiMovie)));
     }
 
+    editMovie(authorEmail: string | undefined, title: string, genre: string, description: string, imageUrl: string, releaseDate: Date, movieId: string | null): Observable<Movie> {
+        let movie = {
+            authorEmail,
+            title,
+            genre,
+            description,
+            img: imageUrl,
+            releaseDate
+        }
+
+        return this.httpClient.put<ApiMovie>(`${this.baseUrl}/movies/${movieId}`, movie).pipe(map((apiMovie: ApiMovie) => this.mapApiMovieToMovie(apiMovie)));
+    }
+
     deleteMovie(id: string | null): Observable<any> {
         return this.httpClient.delete(`${this.baseUrl}/movies/${id}`);
     }
-
-
 
     private mapApiMovieToMovie(apiMovie: ApiMovie): Movie {
         return {
@@ -52,7 +63,7 @@ export class MovieService {
             description: apiMovie.description,
             imageUrl: apiMovie.img,
             genre: apiMovie.genre,
-            releaseDate: new Date(apiMovie.releaseDate),
+            releaseDate: apiMovie.releaseDate,
             _id: apiMovie._id
         }
     }
