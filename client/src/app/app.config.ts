4ba5dd5043a60c268e9599a46ec0f08aaac507a2
Viktor <viktorsarafov1@abv.bg>
2025-08-11 00:37:23 +0300

    Move folder interceptors to core folder and update import in app.config.ts

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { AuthInterceptor } from './interceptors/auth-interceptor';
+import { AuthInterceptor } from './core/interceptors/auth-interceptor';
 // import { AuthInterceptor } from './interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
