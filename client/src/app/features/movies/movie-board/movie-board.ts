2ac67c2eeb17b95479639f6e547bf082a9fb9a2e
Viktor <viktorsarafov1@abv.bg>
2025-08-11 00:39:05 +0300

    Implement search logic in movie-board.ts

--- client/src/app/features/movies/movie-board/movie-board.ts
+++ client/src/app/features/movies/movie-board/movie-board.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal, WritableSignal } from '@angular/core';
 import { Movie } from '../../../models';
 import { MovieService } from '../../../core/services/movie.service';
 import { CommonModule } from '@angular/common';
@@ -14,12 +14,25 @@ import { MovieItem } from '../movie-item/movie-item';
 })
 export class MovieBoard implements OnInit, OnDestroy {
   public movies: Movie[] = [];
+  public filteredMovies: Movie[] = [];
   private subscriptions: Subscription[] = [];
   constructor(private movieService: MovieService) { }
-  
+
+  public searchHandler(event: Event): void {
+    let searchTerm = (event.target as HTMLInputElement).value;
+    console.log(searchTerm);
+
+    let resultMovies = this.movies.filter((movie: Movie) => {
+      return movie.title.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+
+    this.filteredMovies = resultMovies;
+  }
+
   ngOnInit(): void {
     this.subscriptions.push(this.movieService.getMovies().subscribe((response: Movie[]) => {
       this.movies = response;
+      this.filteredMovies = this.movies;
     }));
   }
 
