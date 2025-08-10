ca726d5793473404721f286d2d4e9ed108433312
Viktor <viktorsarafov1@abv.bg>
2025-08-10 03:08:44 +0300

    Create UserService to handle like requests

---
+++ client/src/app/core/services/user.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { map, Observable, tap } from "rxjs";
+import { Like } from "../../models/like.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+    private baseUrl: string = 'http://localhost:3030/data';
+
+    constructor(private httpClient: HttpClient) { }
+
+    like(ownerId: string | null, movieId: string | null): Observable<any> {
+        return this.httpClient.post(`${this.baseUrl}/likes`, { ownerId, movieId });
+    }
+
+    getLikes(movieId: string | null): Observable<number> {
+        return this.httpClient.get<Like[]>(`${this.baseUrl}/likes`).pipe(map((response: Like[]) => {
+            response = response.filter(curEl => curEl.movieId === movieId);
+
+            return response.length;
+        })
+        );
+    }
+}
