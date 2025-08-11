084ceb6001e321870a7ffec29d2b8244a28019c1
Viktor <viktorsarafov1@abv.bg>
2025-08-12 02:57:29 +0300

    Delete Edit button in Profile Component

--- client/src/app/features/profile/profile.ts
+++ client/src/app/features/profile/profile.ts
@@ -6,7 +6,7 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './profile.html',
   styleUrl: './profile.css'
 })
