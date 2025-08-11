050b4191f1287f0e4ec4b40e7d92540c63a70376
Viktor <viktorsarafov1@abv.bg>
2025-08-11 06:07:34 +0300

    Implement logic in Profile Component

---
+++ client/src/app/features/profile/profile.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../core/services/user.service';
+import { User } from '../../models';
+import { Subscription } from 'rxjs';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-profile',
+  imports: [RouterLink],
+  templateUrl: './profile.html',
+  styleUrl: './profile.css'
+})
+export class Profile implements OnInit {
+  public user: User | null = null;
+  private subscriptions: Subscription[] = []
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.subscriptions.push(this.userService.getUser().subscribe((response: User) => {
+      this.user = response;
+    }));
+  }
+}
